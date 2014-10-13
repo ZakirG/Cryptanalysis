2f989b7589b2334735d749d7a6199a44da61d58b
ZakirG <zakirgowani@gmail.com>
2014-10-12 19:57:13 -0500

    Moderate success in cracking the caesar cipher; working towards cracking vigenere cipher

--- Ciphers.java
+++ Ciphers.java
@@ -3,18 +3,20 @@ import java.util.*;
 import java.io.*;
 
 public class Ciphers {
-	public static String readFile( String file ) throws IOException {
-    	BufferedReader reader = new BufferedReader( new FileReader (file));
-    	String         line = null;
-    	StringBuilder  stringBuilder = new StringBuilder();
-    	String         ls = System.getProperty("line.separator");
 	
-	    while( ( line = reader.readLine() ) != null ) {
-	        stringBuilder.append( line );
-	        stringBuilder.append( ls );
-	    }
-
-    	return stringBuilder.toString();
+	// Credit to stack overflow user OscarRyz for this file reading function
+	public static String readFile(String file) throws IOException {
+    		BufferedReader reader = new BufferedReader( new FileReader (file));
+    		String line = null;
+    		StringBuilder stringBuilder = new StringBuilder();
+    		String ls = System.getProperty("line.separator");
+		    
+		    while(( line = reader.readLine()) != null ) {
+		        stringBuilder.append(line);
+		        stringBuilder.append(ls);
+		    }
+		    
+    		return stringBuilder.toString();
 	}
 	
 	public static char[] alphabet = { 
